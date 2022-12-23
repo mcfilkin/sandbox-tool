732b9400b7cb5513255b1c87967e6cddd9398f15
mcfilkin <mcfilkin@gmail.com>
2022-12-24 00:36:12 +0300

    Add some to .gitignore

---
+++ src/main/java/com/mcfilkin/App.java
@@ -0,0 +1,12 @@
+package com.mcfilkin;
+
+/**
+ * Привет!
+ *
+ * @author mcfilkin
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
